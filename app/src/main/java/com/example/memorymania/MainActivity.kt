816993b321dfe4a45dcd402ac93fd986903c8dc1
vivianne-sonnerborg <vivvi.sonnerborg@hotmail.com>
2024-11-20 16:33:01 +0100

    Added PlayGameActivity to AndroidManifest.xml
    - Declared PlayGameActivity in the AndroidManifest.xml to ensure proper activity navigation.
    
    - Configured PlayGameActivity as an entry in the manifest for the app's navigation flow.
    
    - Ensured the app can recognize and launch PlayGameActivity when the "Play Game" button is pressed in MainActivity.
    
    - Added new gl hf string to strings.xml.
    
    - Changed to correct Background color and text color in activity_play_game.xml

--- app/src/main/java/com/example/memorymania/MainActivity.kt
+++ app/src/main/java/com/example/memorymania/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.memorymania
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Hitta knappen för att spela spelet
+        val playGameButton = findViewById<Button>(R.id.playGameButton)
+
+        // Sätter en click listener för att starta PlayGameActivity när knappen trycks och skapar en Intent för att starta PlayGameActivity.
+        playGameButton.setOnClickListener {
+            val intent = Intent(this, PlayGameActivity::class.java)
+            startActivity(intent)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/memorymania/PlayGameActivity.kt
@@ -0,0 +1,12 @@
+package com.example.memorymania
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_game) // Sätter xml för Play Game activity när knappen trycks.
+    }
+}
